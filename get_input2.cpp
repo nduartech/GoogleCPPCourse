7ac93a4f98791cdbe2b9d1382543774ffc1769c8
Nathan Duarte <nduartech101@gmail.com>
2017-01-13 10:09:11 -0500

    Getting input in C++

---
+++ get_input2.cpp
@@ -0,0 +1,33 @@
+// get_input2.cpp: Maggie Johnson
+// Description: Illustrate the use of cin to get input
+// and how to recover from errors.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int input_var = 0;
+  // Enter the do while loop and stay there until either
+  // a non-numeric is entered, or -1 is entered.  Note that
+  // cin will accept any integer, 4, 40, 400, etc.
+  do {
+    cout << "Enter a number (-1 = quit): ";
+    // The following line accepts input from the keyboard into
+    // variable input_var.
+    // cin returns false if an input operation fails, that is, if
+    // something other than an int (the type of input_var) is entered.
+    if (!(cin >> input_var)) {
+      cout << "Please enter numbers only." << endl;
+      cin.clear();
+      cin.ignore(10000,'\n');
+    }
+    if (input_var != -1) {
+      cout << "You entered " << input_var << endl;
+    }
+  }
+  while (input_var != -1);
+  cout << "All done." << endl;
+
+  return 0;
+}
